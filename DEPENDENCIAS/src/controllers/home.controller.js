04e008f78fa2d25e80fcc52a8ed34fe230631246
Rafa2408 <rafaelrosero.r.r2408@gmail.com>
2023-07-16 16:48:48 -0500

    cambios por el body-parser

--- DEPENDENCIAS/src/controllers/home.controller.js
+++ DEPENDENCIAS/src/controllers/home.controller.js
@@ -1,3 +1,4 @@
+import app from '../app.js';
 import {pool} from '../db.js';
 //import {app} from "../app.js";
 
@@ -16,27 +17,70 @@ export const PostRegistro_Suelos = async(req, res) => {
 // Manejo de la solicitud POST para obtener los datos del formulario
 export const postFisicas = async (req, res) =>
 {
+    /*
     const {
         apparent_density,
         real_density,
         relative_density,
         maximum_dry_density,
-        compressive_strength 
+        compressive_strength,
+        thermal_conductivity,
+        liquid,
+        plastic,
+        silt,
+        clay,
+        gravel,
+        sand,
+        optimum_moisture_content,
+        plasticity_index,
+        gran_size,
+        water_content,
+        color,
+        tensile_strength,
+        porosity,
+        initial_moisture,
+        earring,
+        ground_altitude,
+        average_temperature,
+        rainfall_regime
     } = req.body;
-
-    /*-
-    let pparent_density = apparent_density
-    let eal_density = real_density
-    let elative_density = relative_density
-    let aximum_dry_density = maximum_dry_density
-    let ompressive_strength = compressive_strength
     */
 
+    var apparent_density = parseFloat(req.body.apparent_density)
+    var real_density = parseFloat(req.body.real_density)
+    var relative_density = parseFloat(req.body.relative_density)
+    var maximum_dry_density = parseFloat(req.body.maximum_dry_density)
+    var compressive_strength = parseFloat(req.body.compressive_strength)
+
+/*
+    thermal_conductivity,
+    liquid,
+    plastic,
+    silt,
+    clay,
+    gravel,
+    sand,
+    optimum_moisture_content,
+    plasticity_index,
+    gran_size,
+    water_content,
+    color,
+    tensile_strength,
+    porosity,
+    initial_moisture,
+    earring,
+    ground_altitude,
+    average_temperature,
+    rainfall_regime*/
+
+    console.log("tipo de datooooo",req.body.apparent_density)
+
+    
     var query = `INSERT INTO PHYSICAL_PROPERTIES(apparent_density, real_density, relative_density, maximum_dry_density, compressive_strength) 
-                    VALUES('${apparent_density}', '${real_density}', '${relative_density}', '${maximum_dry_density}', '${compressive_strength}')
-                    
-                    `
-                    
+                    VALUES('${apparent_density}', '${real_density}', '${relative_density}', '${maximum_dry_density}', '${compressive_strength}'
+
+                    )   
+                    ` 
                     //console.log('Query maked')
     try {
         const result = await pool.query(query)
