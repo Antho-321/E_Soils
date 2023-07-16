3f13385f995b8fd508a9254879d252848e82502a
Antho-321 <anthonyluisluna225@gmail.com>
2023-07-16 16:50:31 -0500

    cambios en rama

--- DEPENDENCIAS/src/controllers/home.controller.js
+++ DEPENDENCIAS/src/controllers/home.controller.js
@@ -1,53 +1,20 @@
+import express from 'express';
 import {pool} from '../db.js';
-//import {app} from "../app.js";
+var bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
 
 export const rutaPricipal = async(req,res) => {
     const resultadopeticion = await pool.query("SELECT * FROM clientes");
         res.json(resultadopeticion)
 };
 
-
-//#region FUNCTIONS
 export const PostRegistro_Suelos = async(req, res) => {
 
 };
 
-
-// Manejo de la solicitud POST para obtener los datos del formulario
 export const postFisicas = async (req, res) =>
 {
-    const {
-        apparent_density,
-        real_density,
-        relative_density,
-        maximum_dry_density,
-        compressive_strength 
-    } = req.body;
-
-    /*-
-    let pparent_density = apparent_density
-    let eal_density = real_density
-    let elative_density = relative_density
-    let aximum_dry_density = maximum_dry_density
-    let ompressive_strength = compressive_strength
-    */
-
-    var query = `INSERT INTO PHYSICAL_PROPERTIES(apparent_density, real_density, relative_density, maximum_dry_density, compressive_strength) 
-                    VALUES('${apparent_density}', '${real_density}', '${relative_density}', '${maximum_dry_density}', '${compressive_strength}')
-                    
-                    `
-                    
-                    //console.log('Query maked')
-    try {
-        const result = await pool.query(query)
-        //console.log('Query Done')
-        //return 'Query done'
-        res.status(200).json(result)
-
-    } catch (error) {
-        console.error("Error en la consulta:", error);
-        throw error;
-    }
-}
-
-//#endregion
+    let apparent_density=req.body.apparent_density;
+    console.log(apparent_density);
+ }
