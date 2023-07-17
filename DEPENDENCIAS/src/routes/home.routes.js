14eb925f68c76d576d25dfe4dc2e8fd38dd341c3
Rafa2408 <rafaelrosero.r.r2408@gmail.com>
2023-07-16 21:05:52 -0500

    Físicas y Químicas funcionando

--- DEPENDENCIAS/src/routes/home.routes.js
+++ DEPENDENCIAS/src/routes/home.routes.js
@@ -2,10 +2,11 @@ import { Router } from "express";
 const router = Router();
 
 
-import { rutaPricipal, postFisicas, PostRegistro_Suelos } from "../controllers/home.controller.js";
+import { rutaPricipal, postFisicas, PostRegistro_Suelos, postQuimicas } from "../controllers/home.controller.js";
 router.get('/',rutaPricipal);
 
 
 router.post('/postFisicas', postFisicas);
+router.post('/postQuimicas', postQuimicas);
 
 export default router;
