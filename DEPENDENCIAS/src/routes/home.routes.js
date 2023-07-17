b3d8630c76dd71f6b1c6e6f277edd8832d230f85
Antho-321 <anthonyluisluna225@gmail.com>
2023-07-17 17:47:27 -0500

    cambios_routes

--- DEPENDENCIAS/src/routes/home.routes.js
+++ DEPENDENCIAS/src/routes/home.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 const router = Router();
 
 
-import { rutaPricipal, postFisicas, PostRegistro_Suelos, postQuimicas, postClasification, postRegistro_Usuario2, postRegistro_Usuario1,putClient } from "../controllers/home.controller.js";
+import { rutaPricipal, postFisicas, PostRegistro_Suelos, postQuimicas, postClasification, postRegistro_Usuario2, postRegistro_Usuario1 } from "../controllers/home.controller.js";
 router.get('/',rutaPricipal);
 
 router.post('/postRegistro_Usuario2', postRegistro_Usuario2);
@@ -12,5 +12,5 @@ router.post('/postFisicas', postFisicas);
 router.post('/postQuimicas', postQuimicas);
 router.post('/postClasification', postClasification);
 
-router.put('/putClient', putClient);
+
 export default router;
